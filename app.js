37c8357aadd527600f0801a9656038395c770627
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-08-14 06:57:08 +0500

    js complete

---
+++ app.js
@@ -0,0 +1,25 @@
+let time = document.getElementById("time");
+let month = document.getElementById("month");
+let week = document.getElementById("week");
+let day = document.getElementById("day");
+let hours = document.getElementById("hours");
+let mint = document.getElementById("mint");
+let sec = document.getElementById("sec");
+let date = new Date();
+let birth = new Date("1947-08-14")
+let defrance = date - birth;
+let totalyear = Math.floor(defrance / (1000 * 60 * 60 * 24 * 365.25));
+let totalmonth = Math.floor(defrance / (1000 * 60 * 60 * 24 * 30));
+let totalweeks = Math.floor(defrance / (1000 * 60 * 60 * 24 * 7));
+let totaldays = Math.floor(defrance / (1000 * 60 * 60 * 24));
+let totalhours = Math.floor(defrance / (1000 * 60 * 60));
+let totalmint = Math.floor(defrance / (1000 * 60));
+let totalsec = Math.floor(defrance / (1000));
+console.log(totalyear)
+time.innerHTML = `Total years ${totalyear}`
+month.innerHTML = `Total years ${totalmonth}`
+week.innerHTML = `Total years ${totalweeks}`
+day.innerHTML = `Total years ${totaldays}`
+hours.innerHTML = `Total years ${totalhours}`
+mint.innerHTML = `Total years ${totalmint}`
+sec.innerHTML = `Total years ${totalsec}`
