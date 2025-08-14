29db206be3c4c12f1ccad56fa37fc0b38cf1ef22
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-08-14 07:16:15 +0500

    complete

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let time = document.getElementById("time");
+setInterval(() => {
+    let time = document.getElementById("time");
 let month = document.getElementById("month");
 let week = document.getElementById("week");
 let day = document.getElementById("day");
@@ -6,20 +7,24 @@ let hours = document.getElementById("hours");
 let mint = document.getElementById("mint");
 let sec = document.getElementById("sec");
 let date = new Date();
-let birth = new Date("1947-08-14")
+let birth = new Date("1947-08-14T23:30:00")
 let defrance = date - birth;
 let totalyear = Math.floor(defrance / (1000 * 60 * 60 * 24 * 365.25));
-let totalmonth = Math.floor(defrance / (1000 * 60 * 60 * 24 * 30));
+let totalmonth = Math.floor(defrance / (1000 * 60 * 60 * 24 * 30.4375));
 let totalweeks = Math.floor(defrance / (1000 * 60 * 60 * 24 * 7));
 let totaldays = Math.floor(defrance / (1000 * 60 * 60 * 24));
 let totalhours = Math.floor(defrance / (1000 * 60 * 60));
 let totalmint = Math.floor(defrance / (1000 * 60));
 let totalsec = Math.floor(defrance / (1000));
 console.log(totalyear)
-time.innerHTML = `Total years ${totalyear}`
-month.innerHTML = `Total years ${totalmonth}`
-week.innerHTML = `Total years ${totalweeks}`
-day.innerHTML = `Total years ${totaldays}`
-hours.innerHTML = `Total years ${totalhours}`
-mint.innerHTML = `Total years ${totalmint}`
-sec.innerHTML = `Total years ${totalsec}`
+time.innerHTML = ` ${totalyear}`
+month.innerHTML = ` ${totalmonth}`
+week.innerHTML = `${totalweeks}`
+day.innerHTML = ` ${totaldays}`
+hours.innerHTML = `${totalhours}`
+mint.innerHTML = ` ${totalmint}`
+sec.innerHTML = ` ${totalsec}`
+if(totalmint == 41024657){
+    alert(`yes!`)
+}
+}, 1000);
